30ebec345b54a3bb58486d1ed2bd17534464f054
Adham <adhammansour886@gmail.com>
2022-09-14 03:49:36 +0200

    First commit

---
+++ components/view-source.tsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2020 Vercel Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import styles from './view-source.module.css';
+import { REPO } from '@lib/constants';
+
+export default function ViewSource() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="72"
+      height="72"
+      viewBox="0 0 250 250"
+      fill="#151513"
+      className={styles.svg}
+    >
+      <a title="View Source" href={REPO} target="_blank" rel="noopener noreferrer">
+       </a>
+    </svg>
+  );
+}
